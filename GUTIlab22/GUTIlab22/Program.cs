209842a1dc2a16458577d0e5d61aa17a33650e99
XoXoWacky <Admin@DESKTOP-FKO6PH7>
2019-02-15 14:53:39 +0700

    second

--- GUTIlab22/GUTIlab22/Program.cs
+++ GUTIlab22/GUTIlab22/Program.cs
@@ -1,4 +1,35 @@
-﻿using System;
+﻿
+using System;
+namespace HelloWorld
+{
+    class Hello
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
+using System;
+namespace HelloWorld
+{
+    class Hello
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
+using System;
 namespace HelloWorld
 {
     class Hello
